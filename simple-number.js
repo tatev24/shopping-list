1abaab01507e095ee1b599dc90daab8557c0d7e2
tatev24 <tatogab24@gmail.com>
2020-10-11 21:59:35 +0400

    Tab lines 4-9 and clear comments

--- simple-number.js
+++ simple-number.js
@@ -1,20 +1,13 @@
 function checkFunction(number){
-  console.log(number);
 
-for (i = 2 ; i < number - 1; i++) {
-  var b = number % i;
-  if(b == 0){
-    return number + ' is not simple';
+  for (i = 2 ; i < number - 1; i++) {
+    var b = number % i;
+      if(b == 0){
+        return number + ' is not simple';
+      }
   }
-}
     return number + ' is simple';
 
-  // for i = 2 ;i < number - 1 {
-  //   var b = number % i ;
-  //   if b == 0
-  //     return number + " is not a simple";
-  //   not use // else {}
-  // }
 }
 
 var c = checkFunction(15);
