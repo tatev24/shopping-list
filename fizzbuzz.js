2162553995fd8954347ba56a21f7e3661035e1ce
tatev24 <tatogab24@gmail.com>
2020-10-13 15:20:13 +0400

    FizzBuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,17 @@
+function thisFunction(){
+  var numbersArray = [];
+  for (var i = 1; i < 101; i++){
+    if  (i % 3 == 0 && i % 5 == 0) {
+      numbersArray.push("fizzbuzz");
+    }else if (i % 3 == 0) {
+      numbersArray.push("fizz");
+    }else if (i % 5 == 0) {
+      numbersArray.push("buzz");
+    }else {
+      numbersArray.push(i);
+    }
+  }
+  return numbersArray;
+}
+var result = thisFunction();
+console.log(result);
