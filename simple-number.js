7ea2a9b7c39dbde33776c333a90379955f0d7e5b
tatev24 <tatogab24@gmail.com>
2020-10-13 14:12:36 +0400

    Correcting

--- simple-number.js
+++ simple-number.js
@@ -1,13 +1,16 @@
 function checkFunction(number){
-  for (i = 2 ; i < number - 1; i++) {
+  var status = true;
+
+  for (var i = 2 ; i < number - 1; i++) {
     var b = number % i;
 
     if(b == 0){
-      return number + 'false';
+      status = false;
+      break;
     }
   }
 
-  return number + ' true';
+  return status;
 }
 
 // var c = checkFunction(3);
@@ -17,14 +20,21 @@ function showFirstNSimpleNumbers() {
   var maxnumber = 101;
   var simpleNumbersArray = [];
 
-  for (i = 2; i < maxnumber; i++){
-    var c = checkFunction(i);
-    if(c == ' true'){
+  for (var i = 4; i < maxnumber; i++){
+     var c = checkFunction(i);
+     if(c == true){
       simpleNumbersArray.push(i);
-    }
+     }
   }
-  return simpleNumbersArray
+  return simpleNumbersArray;
 }
+
+// RULE number 1
+// misht popoxakan haytarareluc qri var u verj!!!
+// ete ches grum inq@ globala haskanum u et depqum 2 functioni mej nuyn i a grac!
+
+// Braek, Continue karda FOR loop-i hamara
+// U mekel while-ov loop gri lav vorpes orinak! de good night and sweet dreams!okay good night  
 
 
 var result = showFirstNSimpleNumbers();
