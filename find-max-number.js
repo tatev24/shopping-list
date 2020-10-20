ea3eb74c30751f6ce102ce39c78da910d870cdbf
tatev24 <tatogab24@gmail.com>
2020-10-20 16:16:46 +0400

    Added find-max-number.js file

---
+++ find-max-number.js
@@ -0,0 +1,55 @@
+var arr = [5, 7, 33, 9, 15, 26];
+
+function findmaxNumber(numbersArray){
+  var max = numbersArray[0];
+  var min = numbersArray[0];
+
+  for (var i = 0; i < numbersArray.length; i++) {
+    if ( numbersArray[i] > max) {
+       max = numbersArray[i];
+    }
+
+    if ( numbersArray[i] < min){
+      min = numbersArray[i];
+    }
+  }
+  return [max, min];
+}
+
+var x = findmaxNumber(arr);
+console.log(x);
+
+var txt = "Hello world!";
+
+function reverseDirection(str){
+  var empty = "";
+
+  for(var i = str.length - 1; i >= 0; i--){
+    empty = empty + str[i];
+  }
+
+  return empty;
+}
+var a = reverseDirection(txt);
+console.log(a);
+
+
+
+
+var stringText = 'hello my friend! how are you?';
+
+function uppercaseFunction(strInput){
+  var arr = strInput.split(' ');
+  var secondArray = [];
+
+  for (var i = 0; i < arr.length; i++) {
+    var words = arr[i];
+    words = words.charAt(0).toUpperCase() + words.slice(1)
+    secondArray.push(words);
+  }
+
+  return secondArray.join(' ');
+}
+
+var func = uppercaseFunction(stringText);
+console.log(func);
